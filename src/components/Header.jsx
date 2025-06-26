fd1c47f7a57b08534cefac9040a829c2e7036cfa
arcanom <60106527+arcanom@users.noreply.github.com>
2025-06-26 13:27:40 +0200

    add Link in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,14 +25,16 @@ const Header = () => {
 
       <div className="flex gap-2 ">
         <Link to="/inscription">
-            <button className="bg-yellow-300 text-black font-bold px-4 py-2 rounded-xl shadow-sm border border-black">
+            <button className="bg-yellow-300 text-white font-bold px-4 py-2 rounded-xl shadow-sm border border-black">
       Inscription
         </button>
         </Link>
-      
-        <button className="bg-blue-400 text-white font-bold px-4 py-2 rounded-xl shadow-sm border border-black">
+        <Link to="/connexion">
+           <button className="bg-blue-400 text-white font-bold px-4 py-2 rounded-xl shadow-sm border border-black">
           Connexion
         </button>
+        </Link>
+     
       </div>
     </header>
   );
